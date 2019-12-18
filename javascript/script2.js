cb98a2d5ae4202871d2c7c6fe8bc388c2994cc17
alienware <375285942@qq.com>
2019-12-18 23:48:54 +0800

    const关键字

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,5 +1,6 @@
 // 此处的test其实就是defaultexport中的默认导出
 import test from "./defaultexport";
-
-var test2=test();
-
+// const 声明一个只读的常量，一旦声明，常量的值就不能改变
+const test2 = test();
+export default expression
+export default function () {}
