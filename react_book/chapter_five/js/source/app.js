4397f390e9cec1494740823b7523f8118f5aef86
alienware <375285942@qq.com>
2019-12-25 00:08:58 +0800

    项目架构案例

---
+++ react_book/chapter_five/js/source/app.js
@@ -0,0 +1,11 @@
+'use strict'; //使用严格模式
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo'
+
+React.render(
+<h1>
+<Logo /> Welcome to The App!
+</h1>,
+document.getElementById('app')
+);
