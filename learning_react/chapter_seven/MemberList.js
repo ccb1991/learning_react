1c305aa8e08d751b1442a1cb141c4fadd1b74bbd
alienware <375285942@qq.com>
2020-01-16 00:18:43 +0800

    MemberList

--- learning_react/chapter_seven/MemberList.js
+++ learning_react/chapter_seven/MemberList.js
@@ -8,3 +8,55 @@ const getFakeMembers=count=>new Promise((resolves,rejects)=>{
     request.onerror=err=>rejects(err);
     request.send()
 });
+
+const Member=({email,picture,name,location})=>
+    <div className="member">
+        <img src={picture.thumbnail} alt=""/>
+        <h1>{name.first} {name.last}</h1>
+        <p><a href={"mailto:" + email}>{email}</a></p>
+        <p>{location.city}, {location.state}</p>
+    </div>;
+
+class MemberList extends Component{
+    constructor() {
+        super();
+        this.state={
+            members:[],
+            loading:false,
+            error:null
+        }
+    }
+
+    componentWillMount(){
+        this.setState({loading:true});
+        getFakeMembers(this.props.count).then(
+            members=>{
+                this.setState({members,loading:false})
+            },
+            error=>{
+                this.setState({error,loading:false})
+            }
+        )
+    }
+
+    componentWillUpdate(){
+        console.log("updating lifecycle")
+    }
+
+    render(){
+        const {members,loading,error}=this.state
+        return(
+            <div className="member-list">
+                {(loading)?
+                    <span>Loading Members</span>:
+                    (members.length)?
+                        members.map((user,i)=>
+                        <Member key={i} {...user}/>
+                        ):
+                        <span>0 members loaded...</span>
+                }
+                {(error)?<p>Error Loading Members:error</p>:""}
+            </div>
+        )
+    }
+}
